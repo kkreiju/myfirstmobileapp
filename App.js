ef786dd371fadd986ae9dc5d4dd3d3b2d5f9e180
kkreiju <138178348+kkreiju@users.noreply.github.com>
2024-03-29 16:07:37 +0800

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Image } from 'react-native';
+import ucImage from './pictures/uclogo.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>UC LOGIN</Text>
+      <Image source={ucImage} style={{ width: 300, height: 150 }} />
       <StatusBar style="auto" />
     </View>
   );
